234bea389f04790a3f6e690490246d4dc10ffd08
Wenjun Che <wenjun@openf.in>
2016-03-08 18:15:35 -0500

    RUN-1731: issue with sendKey

--- src/main/java/com/openfin/InputTest.java
+++ src/main/java/com/openfin/InputTest.java
@@ -1,7 +1,238 @@
 package com.openfin;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
- * Created by richard on 3/8/16.
+ * Test an issue with sendKey to input fields
+ *
+ * Created by wche on 3/8/16.
  */
 public class InputTest {
+
+    /**
+     * main method
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        String execPath = System.getProperty("ExecPath");   // path to OpenFinRVM.exe
+        String execArgs = System.getProperty("ExecArgs");   // command arguments for OpenFinRVM.exe, such as --config="app.json"
+        String debuggerAddress = System.getProperty("DebuggerAddress");  // debugger URL for OpenFin Runtime.exe
+        String remoteDriverURL = System.getProperty("RemoteDriverURL");  // URL to Selenium server or chromedriver
+        WebDriver driver;
+
+        ChromeOptions options = new ChromeOptions();
+        if (execPath != null) {
+            System.out.println("Binary path " + execPath);
+            options.setBinary(new File(execPath));
+            options.addArguments(execArgs);
+        }
+        options.setExperimentalOption("debuggerAddress", debuggerAddress != null ? debuggerAddress : "localhost:9090");
+        options.setExperimentalOption("forceDevToolsScreenshot", Boolean.TRUE); // required for saving screenshot
+
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY,  options);
+
+        System.out.println("Creating remote driver " + remoteDriverURL);
+        driver = new RemoteWebDriver(new URL(remoteDriverURL), capabilities);
+
+        System.out.println("Got the driver " + driver.getCurrentUrl());
+        driver.manage().timeouts().setScriptTimeout(3, TimeUnit.SECONDS);
+
+        try {
+            if (switchWindow(driver, "Hello OpenFin")) {  // select main window
+                sleep(1);
+            }
+
+            for (int i = 0; i < 10; i++) {
+                String text = String.format("Loop %d", i);
+                populateInputs(driver, text);
+                checkInputs(driver, text);
+            }
+
+            executeJavascript(driver, "fin.desktop.System.exit();");  // ask OpenFin Runtime to exit
+            sleep(2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private static  void populateInputs(WebDriver driver, String text) {
+        WebElement container = driver.findElement(By.id("container"));
+        List<WebElement> list = container.findElements(By.cssSelector("input.text-box"));
+        System.out.println(String.format("populating input of %d elements", list.size()));
+        for (WebElement element: list) {
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    private static  void checkInputs(WebDriver driver, String text) {
+        WebElement container = driver.findElement(By.id("container"));
+        List<WebElement> list = container.findElements(By.cssSelector("input.text-box"));
+        System.out.println(String.format("checking input of %d elements", list.size()));
+        for (WebElement element: list) {
+            String elementText = element.getAttribute("value");
+            if (!text.equals(elementText)) {
+                System.out.println(String.format("Value mismatch %s %s", elementText, text));
+            }
+        }
+    }
+
+
+    /**
+     * target a window by title
+     *
+     * @param webDriver instance of WebDriver
+     * @param windowTitle title to match
+     * @return true if successful
+     * @throws Exception
+     */
+    private static boolean switchWindow(WebDriver webDriver, String windowTitle) throws Exception {
+        boolean found = false;
+        long start = System.currentTimeMillis();
+        while (!found) {
+            for (String name : webDriver.getWindowHandles()) {
+                try {
+                    webDriver.switchTo().window(name);
+                    if (webDriver.getTitle().equals(windowTitle)) {
+                        found = true;
+                        break;
+                    }
+                } catch (NoSuchWindowException wexp) {
+                    // some windows may get closed during Runtime startup
+                    // so may get this exception depending on timing
+                    System.out.println("Ignoring NoSuchWindowException " + name);
+                }
+            }
+            Thread.sleep(1000);
+            if ((System.currentTimeMillis() - start) > 5*1000) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println(windowTitle + " not found");
+        }
+        return found;
+    }
+
+    /**
+     *
+     * retrieve version of OpenFin Runtime on currently targeted Window
+     *
+     * @param driver instance of WebDriver
+     *
+     */
+    private static void findRunimeVersion(WebDriver driver) {
+        Object response = executeAsyncJavascript(driver,
+                "var callback = arguments[arguments.length - 1];" +
+                        "fin.desktop.System.getVersion(function(v) { callback(v); } );");
+        System.out.println("OpenFin Runtime version " + response);
+    }
+
+    /**
+     * Executes JavaScript in the context of the currently selected window
+     *
+     * @param driver instance of WebDriver
+     * @param script javascript to run
+     * @return
+     */
+    private static Object executeJavascript(WebDriver driver, String script) {
+        System.out.println("Executing javascript: " + script);
+        return ((JavascriptExecutor) driver).executeScript(script);
+    }
+
+    /**
+     * Execute an asynchronous piece of JavaScript in the context of the currently selected frame or
+     * window.
+     *
+     * @param driver instance of WebDriver
+     * @param script javascript to run
+     * @return result of execution
+     */
+    private static Object executeAsyncJavascript(WebDriver driver, String script) {
+        System.out.println("Executing Async javascript: " + script);
+        return ((JavascriptExecutor) driver).executeAsyncScript(script);
+    }
+
+    /**
+     * Sleep
+     * @param secs
+     */
+    private static void sleep(long secs) {
+        System.out.println("Sleeping for " + secs + " seconds");
+        try {
+            Thread.sleep(secs * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Main page of Hello OpenFin app
+     */
+    public static class HelloMainPage {
+        private WebDriver driver;
+        @FindBy(id="desktop-notification")
+        private WebElement notificationUpElement;
+        @FindBy(id="cpu-info")
+        private WebElement cpuInfoElement;
+
+        public HelloMainPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        /**
+         * Click Notification button
+         */
+        public void showNotification() {
+            System.out.println("Showing a notification...");
+            this.notificationUpElement.click();
+        }
+
+        /**
+         * Click CPU Info button to show CPU info window
+         */
+        public void showCPUInfo() {
+            System.out.println("Bring up CPU info page...");
+            this.cpuInfoElement.click();
+        }
+    }
+
+    /**
+     *  CPU info of Hollo OpenFin app
+     */
+    public static class CPUInfoPage {
+        private WebDriver driver;
+        @FindBy(id="close-app")
+        private WebElement closeElement;
+
+        public CPUInfoPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        /**
+         * Click on X button to close the window
+         */
+        public void closePage() {
+            System.out.println("Closeing CPU Info page");
+            this.closeElement.click();
+        }
+    }
+
+
+
 }
