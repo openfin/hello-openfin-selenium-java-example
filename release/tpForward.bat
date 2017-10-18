53b6d80caeb4d39d3edc7a8bbfc009c3d8adea80
Wenjun Che <wenjun@openfin.co>
2017-10-18 17:31:04 -0400

    AUT-80: updated README

--- release/tpForward.bat
+++
@@ -1,1 +0,0 @@
-java -DRemoteDriverURL=http://localhost:9515 -DExecPath=openfin-installer.exe -DExecArgs=--config="https://demoappdirectory.openf.in/desktop/config/apps/OpenFin/HelloOpenFin/shdtrading.json" -DDebuggerAddress=localhost:9090 -Dusername=igable -Dpassword=Test_1234 -classpath hello-openfin-selenium-jar-with-dependencies.jar com.openfin.TpForward
